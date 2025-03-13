b84463147afb3b5d6f634f06710c5d62fba0d93d
Ruben Croall <122880204+Rubbaducky95@users.noreply.github.com>
2025-03-13 02:27:47 +0100

    Update LoRa_receive.ino
    Removed unnecessary stuff

--- LoRa_code/LoRa_receive/LoRa_receive.ino
+++ LoRa_code/LoRa_receive/LoRa_receive.ino
@@ -18,13 +18,6 @@ float estimateError = 1.0;
 float measurementError = 3.0;
 float estimatedRSSI = 0.0;
 
-// SMA filter variables
-const int smaWindowSize = 5; // Size of the SMA window
-float rssiBuffer[smaWindowSize];
-int rssiBufferIndex = 0;
-float rssiSum = 0.0;
-float smaRSSI;
-
 float packetRSSI;
 
 // Variables
@@ -32,7 +25,6 @@ long sbw = 62500; // test 20800, 62500, 250000
 int sf = 7; // test 7, 8, 9, 10, 11, 12
 
 int msgCounter = 0;
-
 
 void setup() {
   //initialize Serial Monitor
@@ -75,15 +67,6 @@ float kalmanFilter(float measurement) {
   estimateError = (1 - kalmanGain) * estimateError;
 
   return estimatedRSSI;
-}
-
-float SMAfilter(float measurement) {
-  // Update the SMA buffer
-    rssiSum -= rssiBuffer[rssiBufferIndex];
-    rssiBuffer[rssiBufferIndex] = measurement;
-    rssiSum += rssiBuffer[rssiBufferIndex];
-    rssiBufferIndex = (rssiBufferIndex + 1) % smaWindowSize;
-    return rssiSum / smaWindowSize;
 }
 
 void updateVariable(String input) {
@@ -144,9 +127,5 @@ void loop() {
     // Apply Kalman Filter
     estimatedRSSI = kalmanFilter(packetRSSI);
     Serial.printf("Kalman RSSI: %.4f\n", estimatedRSSI);
-
-    // Apply SMA filter
-    smaRSSI = SMAfilter(packetRSSI);
-    Serial.printf("SMA RSSI: %.4f\n", smaRSSI);
   }
 }
